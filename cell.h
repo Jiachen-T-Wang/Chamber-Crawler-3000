500e4d2cc8b6d2282aaad58b12125c21475caf44
jasonfan524 <jasonfan524@gmail.com>
2017-07-15 19:48:23 -0400

    cell

--- cell.h
+++ cell.h
@@ -2,20 +2,12 @@
 #define __CELL_H__
 #include "subject.h"
 #include "observer.h"
-
-<<<<<<< HEAD
-class Cell: public Subject, public Observer {//abstract
-   int x;
-   int y;
-public:
-   Cell(int x, int y);
-=======
 #include <string>
 
-class Cell{
+class Cell: public Subject, public Observer {//abstract
   int x;
   int y;
   std::string type; // One of doorway, empty, passage, stairway, tile or wall
->>>>>>> bc38184df845b53598517c5d169be011f50ca6b7
+
 };
 #endif
