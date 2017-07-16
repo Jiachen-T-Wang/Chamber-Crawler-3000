250787e9d60f371acdd8ac9f49bea30f2cf57a13
jasonfan524 <jasonfan524@gmail.com>
2017-07-15 20:14:39 -0400

    cell update

--- cell.h
+++ cell.h
@@ -4,10 +4,15 @@
 #include "observer.h"
 #include <string>
 
+class Object;
 class Cell: public Subject, public Observer {//abstract
   int x;
   int y;
   std::string type; // One of doorway, empty, passage, stairway, tile or wall
-
+  Object *content;
+public:
+  Cell(int x, int y, char c);
+  void notify(Subject &);
 };
+
 #endif
