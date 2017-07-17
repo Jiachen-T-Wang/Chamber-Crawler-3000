e96edde364844b362d4fbc23b33053bd02ce4852
Vicky <zhuyx98@outlook.com>
2017-07-16 23:02:27 -0400

    uml

--- character.h
+++ character.h
@@ -4,22 +4,22 @@
 #include "cell.h"
 #include "object.h"
 class Character: public Object{
-  int HP;
-  int atk;
-  int def;
-
+   int HP;
+   int atk;
+   int def;
+   
 protected:
-  int calcDamage(Character* attacker, Character* defender);
+   int calcDamage(Character* attacker, Character* defender);
 public:
-	Character(int HP, int atk, int def);
-	int getHP();
-	int getAtk();
-  int getDef();
-  void addHP(int hp);
-  void getHurt(int damage);
-  void move();
-  virtual void attack()=0;
-  void checkDead();
+   Character(int HP, int atk, int def);
+   int getHP();
+   int getAtk();
+   int getDef();
+   void addHP(int hp);
+   void getHurt(int damage);
+   void move();
+   virtual void attack()=0;
+   void checkDead();
 };
 
 #endif 
