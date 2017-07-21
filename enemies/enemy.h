8d5afd1f8e1b36d2a44cf47f20dfd77b535fd5f7
Vicky <zhuyx98@outlook.com>
2017-07-20 20:02:46 -0400

    jdjdjd

--- enemies/enemy.h
+++ enemies/enemy.h
@@ -1,34 +1,31 @@
 #ifndef __ENEMY_H__
 #define __ENEMY_H__
 
-#include "character.h"
-#include "player.h"
+#include "../character.h"
 
+class Player;
 class Drow;
 class Vampire;
 class Goblin;
 
 //it should be an abstract class, so make dtor pure virtual
 class Enemy: public Character{
-   
   bool moveable;
 
+  int calcDamage(Player* attacker);
 public:
-
-  Enemy(int HP, int atk, int def, std::string);
-   
+  const std::string type;
+  Enemy(int HP, int atk, int def);
   bool canMove();
 
-  void move();
-   
-   virtual void beAtkBy(Player& p);
-   
+  virtual void move();
+
   virtual void attack(Player* p);
   virtual void attack(Drow* d);
   virtual void attack(Vampire* v);
   virtual void attack(Goblin* g);
   
-  
+  virtual void beAtkBy(Player* p);
   virtual ~Enemy()=0;
 };
 
