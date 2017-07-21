c33b7b090a58c120951fd328fc133b53d52b660c
T326WANG <T326WANG@uwaterloo.ca>
2017-07-21 17:56:22 -0400

    cha

--- character.h
+++ character.h
@@ -13,30 +13,16 @@ protected:
    
    
 public:
-<<<<<<< HEAD
     int getHP();
     int getAtk();
     int getDef();
     
     Character(int HP, int atk, int def);
     virtual void moveTo(Dir dir);
-=======
-   
-   Character(int HP, int atk, int def);
-<<<<<<< HEAD
->>>>>>> 3f06dca506a3425b099df294628fb302dcd5a78f
-
     void getHurt(int damage);
-   
     bool checkDead();
-=======
-
-   void hurt(int damage);
-   
-   bool checkDead();
->>>>>>> 3f06dca506a3425b099df294628fb302dcd5a78f
-   
-   };
+    
+};
 
 #endif
 
