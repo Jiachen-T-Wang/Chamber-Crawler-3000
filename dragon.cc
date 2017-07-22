c80b65533432bcecbcc6c52563aa2674d7e96d11
jasonfan524 <jasonfan524@gmail.com>
2017-07-22 13:30:09 -0400

    大改

--- dragon.cc
+++ dragon.cc
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void Dragon::Dragon(std::shared_ptr<DragonHoard> hoard)
+Dragon::Dragon(std::shared_ptr<DragonHoard> hoard)
 	: Enemy{150, 20, 20, "Dragon"}, hoard{hoard} {}
 
 
