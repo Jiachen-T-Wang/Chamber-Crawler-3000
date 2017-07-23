f337833b1d40612b2113677d972439241c419de2
Vicky <zhuyx98@outlook.com>
2017-07-23 18:56:34 -0400

    maiin

--- player.cc
+++ player.cc
@@ -11,97 +11,97 @@ void Player::changeFloor(){
 int Player::getMaxHP() const { return maxHP; }
 
 void Player::setAtkEffect(int effect){
-    atkEffect += effect;
+   atkEffect += effect;
 }
 void Player::setDefEffect(int effect){
-    defEffect += effect;
+   defEffect += effect;
 }
 
 bool Player::isPlayer() { return true;}
 //bool Player::isEnemy() {return false;}
 
 void Player::addHp(int hpEffect) {
-  HP = (getHP() + hpEffect > maxHP) ? maxHP : getHP() + hpEffect;
+   HP = (getHP() + hpEffect > maxHP) ? maxHP : getHP() + hpEffect;
 }
 
 void Player::resetEffect(){
-    atkEffect = 0;
-    defEffect = 0;
+   atkEffect = 0;
+   defEffect = 0;
 }
 
 // Basic Implementation
 void Player::usePotion(Potion* p){
    string type = p->getType();
    int effect =p->getEffect();
-  if (type == "Hp") addHp(effect);
-  else if (type == "Atk") atkEffect += (effect);
-  else if (type == "Def") defEffect += (effect);
-    getPos()->notifyUsePotion(p);
+   if (type == "Hp") addHp(effect);
+   else if (type == "Atk") atkEffect += (effect);
+   else if (type == "Def") defEffect += (effect);
+   getPos()->notifyUsePotion(p);
 }
 
 void Player::moveTo(Dir dir){
-    Cell* nb = getPos()->getNeighbour(dir);
-    shared_ptr<Object> cont = nb->getContent();
-    if(cont.get() && cont->isTreasure()){
-        Treasure* gd = (Treasure*)cont.get();
-        this->PickGold(gd);
-        nb->setCont(nullptr);
-        
-    } else if(cont.get() && cont->isStair()){
-       changeFloor();
-    } else if(cont.get() == nullptr && nb->canStand()){
-        Character::moveTo(dir);
-        getPos()->notifyPlayerMove(dir);
-    }
-    
-    else{
-        getPos()->notifyCannotMove();
-    }
+   Cell* nb = getPos()->getNeighbour(dir);
+   shared_ptr<Object> cont = nb->getContent();
+   if(cont.get() && cont->isTreasure()){
+      Treasure* gd = (Treasure*)cont.get();
+      this->PickGold(gd);
+      nb->setCont(nullptr);
+      
+   } else if(cont.get() && cont->isStair()){
+      changeFloor();
+   } else if(cont.get() == nullptr && nb->canStand()){
+      Character::moveTo(dir);
+      getPos()->notifyPlayerMove(dir);
+   }
+   
+   else{
+      getPos()->notifyCannotMove();
+   }
 }
 
 int Player::showFloor(){
-    return f->getLevel();
+   return f->getLevel();
 }
 
 
 int Player::realAtk(){
-  return (getAtk() + atkEffect < 0) ? 0 : getAtk() + atkEffect;
+   return (getAtk() + atkEffect < 0) ? 0 : getAtk() + atkEffect;
 }
 
 int Player::realDef(){
-  return (getDef() + defEffect < 0) ? 0 : getDef() + defEffect;
+   return (getDef() + defEffect < 0) ? 0 : getDef() + defEffect;
 }
 
 int Player::calcDamage(Enemy* defender){
-    return ceil((100 * this->realAtk()/(100+defender->getDef())) );
+   return ceil((100 * this->realAtk()/(100+defender->getDef())) );
 }
 
 
 void Player::attack(Enemy* enemy){
-	int damage = calcDamage(enemy);
-	enemy->getHurt(damage);
-	getPos()->notifyPlayerAttack(enemy, damage);
+   int damage = calcDamage(enemy);
+   enemy->getHurt(damage);
+   getPos()->notifyPlayerAttack(enemy, damage);
 }
 
 void Player::attack(Halfling* h){
-	int notMiss = rand() % 2;  
-	if (notMiss){
-        int damage = calcDamage(h);
-		h->getHurt(damage);
-		getPos()->notifyPlayerAttack(h, damage);
-	}
-	else{
-		getPos()->notifyMiss();
-	}
+   int notMiss = rand() % 2;
+   if (notMiss){
+      int damage = calcDamage(h);
+      h->getHurt(damage);
+      getPos()->notifyPlayerAttack(h, damage);
+   }
+   else{
+      getPos()->notifyMiss();
+   }
 }
 
 
 void Player::beAtkBy(Enemy* enemy){
-    enemy->attack(this);
+   enemy->attack(this);
 }
 
 void Player::setFloor(Floor* f){
-    this->f = f;
+   this->f = f;
 }
 
 int Player::showScore(){ return score; }
@@ -109,13 +109,16 @@ int Player::showScore(){ return score; }
 void Player::incScore(int value){ score += value; }
 
 void Player::PickGold(Treasure* gold){
-	incScore(gold->getValue());
-//    Cell* newp = gold->getPos();
-//    newp->setCont(nullptr);
+   incScore(gold->getValue());
+   //    Cell* newp = gold->getPos();
+   //    newp->setCont(nullptr);
 }
 
 void Player::roundChange(){}
 
 char Player::symbol(){return '@';}
+bool Player::getGoToNext(){
+   return goToNext;
+}
 
 Player::~Player() {}
