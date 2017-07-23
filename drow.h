f7c8a577d89591ca34f06308445145182f15d04e
T326WANG <T326WANG@uwaterloo.ca>
2017-07-23 17:19:54 -0400

    d

--- drow.h
+++ drow.h
@@ -12,7 +12,7 @@ class Potion;
 class Drow: public Player{
    public:
    	Drow(Floor* f);
-   	void usePotion(std::shared_ptr<Potion> p) override; //potions have their effect magnified by 1.5
+   	void usePotion(Potion* p) override; //potions have their effect magnified by 1.5
    	void beAtkBy(Enemy* e) override;
 };
 
