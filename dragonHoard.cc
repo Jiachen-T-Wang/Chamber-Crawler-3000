4d511a257d68fd572e287a41242ecd0d3f6833ab
T326WANG <T326WANG@uwaterloo.ca>
2017-07-22 21:42:58 -0400

    sdf

--- dragonHoard.cc
+++ dragonHoard.cc
@@ -4,7 +4,7 @@ DragonHoard::DragonHoard():
     Treasure{"DragonHoard", 6}
     , dragon{make_shared<Dragon>(std::shared_ptr<DragonHoard>(this))} {
     int d = rand() % 8;
-    dragon->setPos(getPos()->getNeighbour(dragon->numToDir(d)));
+    dragon->setPos(this->getPos()->getNeighbour(dragon->numToDir(d)));
 }
 
 std::shared_ptr<Dragon> DragonHoard::getDragon(){
